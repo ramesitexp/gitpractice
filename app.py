d45a97de9bbc65c7ffca88015c4a82a0925543c4
RameshKumar S <154916851+ramesitexp@users.noreply.github.com>
2024-01-22 22:37:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 #additonal scenorio
 
 def sum(a,b):
-<<<<<<< HEAD
     print("hello welcome man")
-=======
-    print("hello")
->>>>>>> 9a8b78aa6506d1ce553c7e03e1fd310916c6342f
     return a+b+c
 
 def mul(a,b):
